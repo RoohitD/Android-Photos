59834a099c93852f8224d22a0a170f9809a71f21
RoohitD <66574477+RoohitD@users.noreply.github.com>
2023-04-18 21:13:13 -0400

    Added AlbumView

---
+++ PhotosApp/app/src/main/java/com/example/photosapp/AlbumView.java
@@ -0,0 +1,14 @@
+package com.example.photosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlbumView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_view);
+    }
+}
